7922fdd669f809762563b05b390651c0f368727e
Caitlin Keefe <ckeefe90@yahoo.com>
2020-08-23 19:07:42 -0400

    updated accordian to use react hooks

--- src/state/Accordian.js
+++ src/state/Accordian.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
-export default class Accordian extends React.Component {
-    static defaultProps = { sections : [] };
-    state = { currentSectionIndex: 0 };
-    handleButtonClick(index) {
-        this.setState({ currentSectionIndex: index })
-    }
-    render() {
-        return (
+export default function Accordian({sections = []}) {
+    const [currentSectionIndex, setCurrentSectionIndex] = React.useState(0)
+    return (
         <div>
             <ul>
-                {this.props.sections.map((section, i) => 
+                {sections.map((section, i) => 
                 <li key={i}>
-                    <button className='title' onClick={() => this.handleButtonClick(i)}>{section.title}</button>
-                    {this.state.currentSectionIndex === i && <p>{section.content}</p>}
+                    <button className='title' onClick={() => setCurrentSectionIndex(i)}>{section.title}</button>
+                    {currentSectionIndex === i && <p>{section.content}</p>}
                 </li>)}
             </ul>
         </div>
-        )
-    }
+    )
 }
+
+// export default class Accordian extends React.Component {
+//     static defaultProps = { sections : [] };
+//     state = { currentSectionIndex: 0 };
+//     handleButtonClick(index) {
+//         this.setState({ currentSectionIndex: index })
+//     }
+//     render() {
+//         return (
+//         <div>
+//             <ul>
+//                 {this.props.sections.map((section, i) => 
+//                 <li key={i}>
+//                     <button className='title' onClick={() => this.handleButtonClick(i)}>{section.title}</button>
+//                     {this.state.currentSectionIndex === i && <p>{section.content}</p>}
+//                 </li>)}
+//             </ul>
+//         </div>
+//         )
+//     }
+// }
